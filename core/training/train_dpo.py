6ce60af81b32c893fcd9a1aea741d3620c67747e
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2025-05-09 13:56:50 -0700

    dpo force use ref model

--- core/training/train_dpo.py
+++ core/training/train_dpo.py
@@ -221,7 +221,6 @@ def build_trainer(cfg: dict, model, ref_model, tokenizer, train_ds, eval_ds):
         bf16=True,
         use_liger_kernel=True,
         load_best_model_at_end=True,
-        force_use_ref_model=True,
         **hf_kwargs,
     )
     #####################################
@@ -235,6 +234,7 @@ def build_trainer(cfg: dict, model, ref_model, tokenizer, train_ds, eval_ds):
         eval_dataset=eval_ds,
         processing_class=tokenizer,
         callbacks=callbacks,
+        force_use_ref_model=True,
     )
 
 
