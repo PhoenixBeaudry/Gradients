95381a96708250293b0f8599071def61357de12a
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2025-05-09 14:21:32 -0700

    init trl config var

--- miner/logic/job_handler.py
+++ miner/logic/job_handler.py
@@ -136,6 +136,7 @@ def _load_and_modify_config(
             [reward_function.reward_func for reward_function in dataset_type.reward_functions], task_id
             )
         config["rl"] = "grpo"
+        config["trl"] = {}
         config["trl"]["beta"] = 0.04
         config["trl"]["max_completion_length"] = 256
         config["trl"]["use_vllm"] = False
