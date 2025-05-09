e7acaf448f50be5560e121a81c01aea73f33524a
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2025-05-08 18:54:55 -0700

    mbs to 32

--- core/training/hpo_optuna.py
+++ core/training/hpo_optuna.py
@@ -32,7 +32,7 @@ MAX_MINUTES_PER_TRIAL = 15
 def sample_space(trial: optuna.Trial, cfg: dict) -> dict:
     params = {
         "learning_rate":               trial.suggest_float("learning_rate", 6e-6, 6e-4, log=True),
-        "micro_batch_size":            trial.suggest_categorical("micro_batch_size", [2, 4, 8, 16]),
+        "micro_batch_size":            trial.suggest_categorical("micro_batch_size", [2, 4, 8, 16, 32]),
         "gradient_accumulation_steps": trial.suggest_categorical("gradient_accumulation_steps", [1, 2, 4, 8]),
         "weight_decay":                trial.suggest_float("weight_decay", 0.0, 0.2),
     }
