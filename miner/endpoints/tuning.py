b69b1762b7b977c9e09ba5f90553bdb107652f74
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2025-05-08 18:19:02 -0700

    4 workers

--- miner/endpoints/tuning.py
+++ miner/endpoints/tuning.py
@@ -32,7 +32,7 @@ from miner.logic.job_handler import create_job_text
 from miner.logic.job_handler import start_tuning_container
 
 
-NUM_WORKERS = 2
+NUM_WORKERS = 4
 
 logger = get_logger(__name__)
 
