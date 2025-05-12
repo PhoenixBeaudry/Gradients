ee9c88db65c74e4c08331184e1cdb8d43e53f623
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2025-05-12 12:58:51 -0700

    add back other tasks

--- core/training/train_grpo.py
+++ core/training/train_grpo.py
@@ -325,6 +325,9 @@ def main():
     logger.info("Starting Full Model Training...")
 
     trainer.train()
+    if not cfg["hpo_run"]:
+        trainer.model.save_pretrained("final_checkpoint")
+        trainer.push_to_hub()
 
 
 
