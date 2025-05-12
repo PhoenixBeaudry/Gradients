efdada574f39f75a409c11ad7cb7a319ba4d7691
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2025-05-12 11:03:25 -0700

    instruct only

--- miner/endpoints/tuning.py
+++ miner/endpoints/tuning.py
@@ -163,9 +163,17 @@ async def task_offer(
             logger.info("Task Type: Instruct")
         if request.task_type == TaskType.DPOTASK:
             logger.info("Task Type: DPO")
+            return MinerTaskResponse(
+                message=f"This endpoint does not currently support that task.",
+                accepted=False
+            )
         if request.task_type == TaskType.GRPOTASK:
             ########### NO GRPO TASKS YET ###########
             logger.info("Task Type: GRPO")
+            return MinerTaskResponse(
+                message=f"This endpoint does not currently support that task.",
+                accepted=False
+            )
 
         
         if request.task_type not in [TaskType.INSTRUCTTEXTTASK, TaskType.DPOTASK, TaskType.GRPOTASK]:
