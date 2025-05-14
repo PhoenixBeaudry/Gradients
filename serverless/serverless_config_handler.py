bda0f98aed7ea2fa66bb7f839e4bbe4fae25d214
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2025-05-13 20:53:49 -0700

    remvoe custom templating

--- serverless/serverless_config_handler.py
+++ serverless/serverless_config_handler.py
@@ -160,19 +160,14 @@ def _process_grpo_dataset_fields(dataset_type: GrpoDatasetType) -> dict:
 
 
 def _process_dpo_dataset_fields(dataset_type: DpoDatasetType) -> dict:
-    type = "user_defined.default"
-    field_prompt = dataset_type.field_prompt
-    field_system = dataset_type.field_system
-    field_chosen = dataset_type.field_chosen
-    field_rejected = dataset_type.field_rejected
-    prompt_format = "{prompt}"
-    chosen_format = "{chosen}"
-    rejected_format = "{rejected}"
-    full_template_config = {"type": type, "field_prompt": field_prompt, "field_system": field_system, 
-                            "field_chosen": field_chosen, "field_rejected": field_rejected, 
-                            "prompt_format": prompt_format, "chosen_format": chosen_format, "rejected_format": rejected_format, "split": "train"}
+    if dataset_type.field_prompt == "prompt":
+        template = "chatml.prompt_pairs"
+    elif dataset_type.field_prompt == "question":
+        template = "chatml.intel"
+    elif dataset_type.field_prompt == "instruction":
+        template = "chatml.argilla"
 
-    return full_template_config
+    return {"type": template, "split": "train"}
 
 
 def _process_instruct_dataset_fields(instruct_type_dict: dict) -> dict:
