2b93bcd6bf375e85107307c15e7fc6fe747779bb
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2025-05-15 16:41:46 -0700

    no dataset num proc in grpo

--- serverless/train_grpo.py
+++ serverless/train_grpo.py
@@ -307,7 +307,6 @@ def build_trainer(cfg: dict, model, tokenizer, train_ds, eval_ds):
         bf16=True,
         use_liger_kernel=True,
         load_best_model_at_end=True,
-        dataset_num_proc=4,
         **hf_kwargs,
     )
     #####################################
