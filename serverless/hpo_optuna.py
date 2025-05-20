e179098de92e6faedb75b0c6334eaeb8a24b31a1
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2025-05-20 12:05:45 -0700

    add lion_8bit as an optimizer to hpo

--- serverless/hpo_optuna.py
+++ serverless/hpo_optuna.py
@@ -36,6 +36,7 @@ def sample_space(trial: optuna.Trial, cfg: dict) -> dict:
 
     if cfg["rl"] == "dpo" or cfg["rl"] == "grpo":
         params = {
+            "optimizer":                   trial.suggest_categorical("adapter", ["adamw_8bit", "lion_8bit"]),
             "adapter":                     trial.suggest_categorical("adapter", ["lora", "None"]),
             "micro_batch_size":   trial.suggest_categorical("micro_batch_size", [2, 4, 8, 16, 32]),
             "learning_rate":               trial.suggest_float("learning_rate", 6e-7, 1e-4, log=True),
@@ -45,12 +46,13 @@ def sample_space(trial: optuna.Trial, cfg: dict) -> dict:
         }
     else:
         params = {
-        "adapter":                     trial.suggest_categorical("adapter", ["lora", "None"]),
-        "micro_batch_size":   trial.suggest_categorical("micro_batch_size", [2, 4, 8, 16, 32]),
-        "learning_rate":               trial.suggest_float("learning_rate", 3e-6, 1e-4, log=True),
-        "gradient_accumulation_steps": trial.suggest_categorical("gradient_accumulation_steps", [1, 2]),
-        "weight_decay":                trial.suggest_float("weight_decay", 0.0, 0.05),
-    }
+            "optimizer":                   trial.suggest_categorical("adapter", ["adamw_8bit", "lion_8bit"]),
+            "adapter":                     trial.suggest_categorical("adapter", ["lora", "None"]),
+            "micro_batch_size":   trial.suggest_categorical("micro_batch_size", [2, 4, 8, 16, 32]),
+            "learning_rate":               trial.suggest_float("learning_rate", 3e-6, 1e-4, log=True),
+            "gradient_accumulation_steps": trial.suggest_categorical("gradient_accumulation_steps", [1, 2]),
+            "weight_decay":                trial.suggest_float("weight_decay", 0.0, 0.05),
+        }
 
     if params["adapter"] == "lora":
         params |= {
